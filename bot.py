99d23acff6139f45fd2dae8a06186bf4d0825df0
joel.page <joel.page@codemasters.com>
2019-09-24 14:53:48 +0100

    Initial Setup - f-strings and on_member_join event
    Changes:
    - Add on_member_join event override
    - use f'string' format instead of concatenation

--- bot.py
+++ bot.py
@@ -16,9 +16,17 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f"""Hi {member.name}, welcome to Supernova's discord server! 
+Please set your nickname to be your in-game character name and leave us a message in #new-arrivals and we will get back to you!"""
+    )
+
 @bot.command(name='create-channel')
 @commands.has_role('Officer')
-async def create_channel(ctx, channel_name='new-channel-' + str(helpers.getTimeInMilliseconds())):
+async def create_channel(ctx, channel_name=f"new-channel-{helpers.getTimeInMilliseconds()}"):
     guild = ctx.guild
     existing_channel = discord.utils.get(guild.channels, name=channel_name)
     if not existing_channel:

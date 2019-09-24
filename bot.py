7350b89ce1e44d4269665a60aaffb851a257ab16
joel.page <joel.page@codemasters.com>
2019-09-24 14:02:09 +0100

    Initial Commit - Add Git Ignore File and bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+# bot.py
+import os
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='create-channel')
+@commands.has_role('Officer')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
+bot.run(TOKEN)

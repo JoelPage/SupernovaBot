59fd56f9ccc12dee68bcd0d6bc216c41da2c37a5
J P <joelpage92@gmail.com>
2019-10-24 09:41:07 +0100

    Commit to get in sync. Huge changes. - Command Logic - Event/Read/Write refactor

--- events.py
+++ events.py
@@ -1,128 +1,298 @@
-import uid
-import helpers
+import unique_identifier as i_uid
+import helpers as i_util
+import xml.etree.ElementTree as i_tree
+import input as i_input
 
-import xml.etree.ElementTree as ET
+import time as i_time
+import datetime as i_datetime
+
+# Parse Time
+
+g_eventsArray = []
+g_eventsTree = None
+g_exit = False
 
 # Testing
-def CreateDummyEvent():
-    print("CreateDummyEvent")
-    eventsArray.clear()
-    firstEvent = CreateNewEvent("First Event")
-    eventsArray.append(firstEvent)
-    dummyEventsTree = CreateTreeFromEventsArray(eventsArray)
-    helpers.xml_helpers.fileWrite(dummyEventsTree, "events.xml")
-    eventsArray.clear()
+def createDummyEvent():
+    print("createDummyEvent")
+    g_eventsArray.clear()
+    firstEvent = createNewEvent("First Event", i_time.time())
+    g_eventsArray.append(firstEvent)
+    dummyEventsTree = createTreeFromEventsArray(g_eventsArray)
+    i_util.xml_helpers.fileWrite(dummyEventsTree, "events.xml")
+    g_eventsArray.clear()
 
 # Core Functions
-def InitialiseEventsSystem():
-    print("InitialiseEventsSystem")
+def initialiseEventsSystem():
+    print("initialiseEventsSystem")
 
-    CreateDummyEvent()
+    # Create Dummy Event To Populate events.xml
+    #createDummyEvent()
+    global g_eventsTree
+    g_eventsTree = i_util.xml_helpers.fileRead("events.xml")
 
-    eventsTree = helpers.xml_helpers.fileRead("events.xml")
-    CreateEventsArrayFromTree(eventsTree.getroot())
+    createEventsArrayFromTree(g_eventsTree.getroot())
 
-def CreateEventsArrayFromTree(treeRoot):
-    print("CreateEventsArrayFromTree")
-    eventsArray.clear()
+def createEventsArrayFromTree(treeRoot):
+    print("createEventsArrayFromTree")
+    g_eventsArray.clear()
     for events in treeRoot.findall("events"):
         for event in events.findall("event"):
-            _eventName = event.find("name").text
-            _uid = event.find("uid").text
-            eventsArray.append(CreateEvent(_uid, _eventName))
+            eventName = event.find("name").text
+            uid = event.find("uid").text
+            start = f"{int(i_time.time())}" #event.find("start").text
+            g_eventsArray.append(createEvent(uid, eventName, start))
 
-def CreateTreeFromEventsArray(_array):
-    print(f"CreateTreeFromEventsArray({_array} Count:{len(_array)}")
-    root = CreateEventTree()
-    for event in _array:
-        AddEventToTree(root, event)
+def createTreeFromEventsArray(array):
+    print(f"createTreeFromEventsArray({array} Count:{len(array)}")
+    root = createEventTree()
+    for event in array:
+        addEventToTree(root, event)
 
     return root
 
-def CreateEventTree():
-    print("CreateEventTree")
-    root = ET.Element('root')
-    ET.SubElement(root, 'events')
+def createEventTree():
+    print("createEventTree")
+    root = i_tree.Element('root')
+    i_tree.SubElement(root, 'events')
     return root
 
-def CreateNewEvent(_name):
-    print(f"CreateNewEvent({_name})")
-    return Event(None, _name)
+def createNewEvent(name, start):
+    #print(f"createNewEvent({name})")
+    return Event(None, name, start)
 
-def CreateEvent(_uid, _name):
-    print(f"CreateNewEvent({_uid}, {_name})")
-    return Event(_uid=_uid, _name=_name)
+def createEvent(uid, name, start):
+    #print(f"createNewEvent({uid}, {name})")
+    return Event(uid, name, start)
 
-def AddEventToTree(treeRoot, event):
-    print("AddEventToTree")
+def addEventToTree(treeRoot, event):
+    print("addEventToTree")
     events = treeRoot.find('events')
-    newEvent = ET.SubElement(events, 'event')
-    eventName = ET.SubElement(newEvent, 'name')
+    newEvent = i_tree.SubElement(events, 'event')
+    eventName = i_tree.SubElement(newEvent, 'name')
     eventName.text = event.name
-    eventUID = ET.SubElement(newEvent, 'uid')
+    eventUID = i_tree.SubElement(newEvent, 'uid')
     eventUID.text = event.uid
+    eventStartTime = i_tree.SubElement(newEvent, 'start_time')
+    eventStartTime.text = f"{event.start}"
 
-def WriteEventsToFile():
-    helpers.xml_helpers.fileWrite(eventsTree, "events.xml")
+def writeEventsToFile(root):
+    print("writeEventsToFile")
+    i_util.xml_helpers.fileWrite(root, "events.xml")
 
-def ReadEventsFromFile():
-    helpers.xml_helpers.fileRead("events.xml")
+def readEventsFromFile():
+    print("readEventsFromFile")
+    i_util.xml_helpers.fileRead("events.xml")
 
-# Initialise - Create File if no Exists
-#              If file does exist load into memory
-#              Keep tree in memory
-#               
-# Functions  - Add Event to Tree
-#              - Report that an event is being created
-#              - Add to in memory tree
-#              - Export tree to file
-#              - Report UID of created event
-#            - List Events
-#              - Convert events to text
-#              - Display text in console
-#              - Display text in discord
-#            - Remove Event from Tree
-#              - Remove from in memory tree
-#              - Export tree to file
-#            - Edit Event
-#              - Find sub element in tree with matching ID
-#              - Change name of event
-#              - Export tree to file
+def findEventByUID(uid):
+    for event in g_eventsArray:
+        if event.uid == uid:
+            return event
 
-# Event Structure
-# UID 
-# Name 
-# Description - Optional 
-# Start Date Time - Optional 
-# End Date Time  - Optional 
+    return None
+
+# Event Object
 class Event(object):
-    
-    def __init__(self, _uid=None, _name=None, _start=None, _end=None):
-        print(f"Event({_uid}, {_name}, {_start}, {_end})")
 
-        if _uid == None:
-            self.uid = uid.get()
+    def __init__(self, uid, name, start, end=None):
+        print(f"Event({uid}, {name}, {start}, {end})")
+        # Required
+        if uid == None:
+            self.uid = i_uid.get()
         else:
-            self.uid = _uid
+            self.uid = uid
 
-        if _name == None:
-            self.name = _uid
-        else:
-            self.name = _name
+        self.name = name
+        self.start = start
+        # Optional
+        self.end = end
+
+def isEnoughArgs(args, count):
+    return len(args) >= count
+
+def processArguments(args):
+    print(f"processArguments({args})")
+    if isEnoughArgs(args, 1):
+        executeCommand(args[0], args[1:])
+    else:
+        print("No input found")
+
+def executeCommand(command, args):
+    print(f"executeCommand({command}, {args})")
+    commandData = commandSwitch(command)
+
+    if commandData == None:
+        print("Unrecognised Command!")
+        return
+
+    if isEnoughArgs(args, commandData[1]):
+        commandData[0](args)
+    else:
+        print("Not enough arguments for command!")
+
+def commandSwitch(command):
+    switch = { 
+    "exit" : (commandExit, 0),
+    "list" : (commandList, 0),
+    "remove" : (commandRemove, 1),
+    "create" : (commandCreate, 2) }
+    return switch.get(command)
+
+def commandExit(args):
+    global g_exit
+    g_exit = True
+
+def commandList(args):
+    print(f"There are {len(g_eventsArray)} events:")
+    for event in g_eventsArray:
+        print(f"{event.uid}:{event.name}")
+
+def commandRemove(args):
+    uid = args[0] # Arg0 is the UID of the event to remove
+    print(f"Removing Event with UID:{uid}")
+    # TODO : Can we use a dictionary instead and remove by index?
+    foundEvent = findEventByUID(uid)
+    if foundEvent == None:
+        print(f"No event found with UID:{uid}") 
+    else:
+        g_eventsArray.remove(foundEvent)
+        tree = createTreeFromEventsArray(g_eventsArray)
+        writeEventsToFile(tree)
+        print(f"Event with UID:{uid} removed!")    
         
-        self.start = _start
-        self.end = _end
-      
-#evnt = Event("test")
-#print("Test Event :")
-#print(evnt.uid)
-#print(evnt.name)
+def parseTimeArgument(arg):
+    # Parse Text Time into Hours and Minutes
+    # 20:30 -> (20, 30)
+
+    splitArg = arg.split(":")
+    if len(splitArg) < 2:
+        print("Failed to parse Start Time")
+        return (0,0)
+    else:
+        hours = int(splitArg[0][0:2])
+        minutes = int(splitArg[1][0:2])
+        
+        # Validate hours and minutes
+        # less than 24 greater than 0
+        # less than 60 greater than 0
+        
+        return (hours, minutes)
+
+    # TODO : Specified AM/PM ?
+    # string ends with am/pm ?
+    # ToUpper    
+
+def commandCreate(args):
+    name = args[0] # Arg0 is the Name of the event
+    startTime = args[1] # Arg1 is the Start Time of the event
+    print(f"Creating Event {name} at {startTime}")
+
+    startTimeTuple = parseTimeArgument(startTime)
+
+    # Create Date with Specified Time
+    nowDateTime = i_datetime.datetime.now()
+    eventStartTime = nowDateTime.replace(hour=startTimeTuple[0], minute=startTimeTuple[1], second=0, microsecond=0)
+    eventStartTimeStamp = eventStartTime.timestamp()
+
+    # Check for duplicate UID
+    maxTries = 5
+    tries = 0
+    validEvent = False
+    newEvent = None
+    while validEvent == False | tries <= maxTries:
+        newEvent = createNewEvent(name, eventStartTimeStamp)
+        foundEvent = findEventByUID(newEvent.uid)
+        if foundEvent == None:
+            validEvent = True
+
+    if newEvent == None:
+        print("Invalid Event")
+        return
+
+    # handle end time first
+    # as non recursive optional argument
+    # parse time, if parse fails
+    # pass rest of args to recursive optional args
+
+    print(f"Handle optional arguments.")
+
+    # recursive function with switch?
+    # start-date date
+    # end-date
+
+
+    commandCreateOptionalArgs(newEvent, args[2:])
+
+    g_eventsArray.append(newEvent)
+    tree = createTreeFromEventsArray(g_eventsArray)
+    writeEventsToFile(tree)
+
+
+
+    return
+
+    # Optional Arguments
+    # For args[2:]
+
+    # We should handle the following args as option arguments
+    # Recursive function maybe?
+    # Create the event and pass it to this recursive function
+
+
+    # Parse End Time if Valid
+
+    # Parse Start Date
+    # If valid replace date in startdate
+
+    # Parse End Date
+    # If valid replace date in startdate
+
+    # Validate Start and End
+    # If start/end has already passed, event is invalid
+    # If no end then time is always valid
+
+    #if nowDateTime.timestamp() > eventStartTimeStamp:
+        # Event has already started.
+        # Is this okay?
+
+    # if nowDateTime.timestamp() > 
+
+    if validEvent == True:
+        
+        writeEventsToFile()
+    else:
+        print("Event was invalid!")
+    
+def commandCreateOptionalArgs(event, args):
+    if len(args) <= 0:
+        return
+    
+    arg = args[0]
 
 # Core
-eventsArray = []
-eventsTree = None
+initialiseEventsSystem()
 
-InitialiseEventsSystem()
+while g_exit == False:
+    # Gather Input
+    print("Waiting for command...")
+    input = i_input.gatherInput()
+    # Split input by space
+    args = input.split(" ")
+    processArguments(args)
 
-eventsTree = CreateTreeFromEventsArray(eventsArray)
+    continue
 
+    # Edit
+    if command == "edit":        
+        print(f"Editing Event {arg1}")
+        foundEvent = findEventByUID(arg1)
+        if foundEvent == None:
+            print(f"No event found with UID {arg1}")
+        else:
+            print(f"Event with UID {arg1} Found!")
+            
+        continue
+
+# Times for all events to allow sorting.
+# Skip (Remove next upcoming event)
+# More event values, Thumbnail, Image, Description, Members
--- xml_helpers.py
+++ xml_helpers.py
@@ -1,13 +1,63 @@
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
+import re as i_re
 
 # https://stackoverflow.com/questions/24813872/creating-xml-documents-with-whitespace-with-xml-etree-celementtree
 # Return a pretty-printed XML string for the Element.
-def prettify(xmlStr):
-    INDENT = "    "
-    rough_string = ET.tostring(xmlStr, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent=INDENT)
+# Tried this function before but it produced strange results so I just wrote my own.
+def prettify(tree):
+    # Read out tree as bytes
+    encoding = 'utf-8'
+    treeAsBytes = ET.tostring(tree, encoding)
+    # Convert to string
+    treeAsString = str(treeAsBytes, encoding)
+    # Remove new lines
+    splitTree = treeAsString.split("\n")
+    treeAsString = "".join(splitTree)
+    # find all words enclosed by triangle braces
+    # WARNING : This is a bit janky, if someone has a name that includes triangle braces
+    # then this logic could break entirely.
+    split = i_re.findall(r"<(.*?)\>", treeAsString)
+
+    # Variables used for producing correct indentation.
+    start = 0               # The index into the string we will start searching.
+    depth = 0               # The depth of the indentation.
+    indentation = "    "    # The spacing used for indentation.
+    lastString = ""         # The previous element name.
+
+    for string in split:
+        # Apply braces to the string to avoid imperfect matches.
+        strToFind = "<" + string + ">"
+        # Find the index of this string from start to len(treeAsString)
+        index = treeAsString.find(strToFind, start, len(treeAsString))
+        # Set the start value past the string
+        start = index + len(strToFind)
+
+        # If this is a closing element reduce the depth by 1
+        if string.startswith("/"):
+            depth -= 1
+            # If this element matches the last then perform the indentation
+            if not lastString == string[1:]:
+                # Split the strings from index, add a new line and indent, then join them together.
+                treeAsString = treeAsString[:index] + "\n" + (indentation * depth) + treeAsString[index:]
+                # Increase the start index by the length of the indentation
+                start = start + len("\n") + len(indentation * depth)
+
+            # Cache this string for the next loop
+            lastString = string[1:]
+        # If this is an opening element
+        else:
+            # and if this is not the first element
+            if depth != 0:
+                # Perform the indentation and increase the start index
+                treeAsString = treeAsString[:index] + "\n" + (indentation * depth) + treeAsString[index:]
+                start = start + len("\n") + len(indentation * depth)
+
+            # Increase the depth and cache the string
+            depth += 1
+            lastString = string
+
+    return treeAsString
 
 # Write xml string to file
 def fileWrite(data, fileName):
